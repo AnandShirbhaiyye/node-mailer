c413aae68dc7fae7673fb3c553f69205a662bfdb
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2022-11-19 13:23:09 +0530

    create frontend folder and added email file

---
+++ client/src/components/Email.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Email.css'
+
+function Email() {
+  return (
+    <>
+    <div className='text-center'>Email</div>
+    </>
+  )
+}
+
+export default Email
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
